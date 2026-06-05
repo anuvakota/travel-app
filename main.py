b629b3e9a52f82214d3fdbe6f27f77ccde0893f9
Anuva Kota <anuvakota@Anuvas-MacBook-Air.local>
2026-06-05 16:31:41 -0400

    hehe

--- main.py
+++ main.py
@@ -20,7 +20,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://globr-ten.vercel.app",
+    ],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_methods=["*"],
     allow_headers=["*"],
 )
